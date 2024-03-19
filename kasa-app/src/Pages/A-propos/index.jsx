86be3b45ff03c63868e9fc6a9fe4204ef6e7d378
Gareth Thomas <thomasgareth3@gmail.com>
2024-03-19 22:17:59 +0100

    feat:scss

--- kasa-app/src/Pages/A-propos/index.jsx
+++ kasa-app/src/Pages/A-propos/index.jsx
@@ -1,5 +1,20 @@
+import data from "../../apropos-data.js";
+import Collapse from "../../Components/Collapse";
+import mountains from "../../assets/mountains.png";
+
 function Apropos() {
-  return <div>A propos</div>;
+  return (
+    <div>
+      <img src={mountains} alt="moutains" className="aPropos-image" />
+      <div className="collapse-container">
+        {data.map((item, index) => (
+          <Collapse title={item.title} key={`${index}-collapse`}>
+            <p>{item.content}</p>
+          </Collapse>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Apropos;
